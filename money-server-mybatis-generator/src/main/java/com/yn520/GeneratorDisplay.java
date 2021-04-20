e2aef435cc02d653002a38e3fb3919445dad9537
hlbf1314 <2436297293@qq.com>
2021-04-20 23:37:57 +0800

    yn520-money mybatis逆向：项目整理，开源第一版

---
+++ money-server-mybatis-generator/src/main/java/com/yn520/GeneratorDisplay.java
@@ -0,0 +1,47 @@
+package com.yn520;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class GeneratorDisplay {
+
+    public void generator(String name) throws Exception {
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        //指定 逆向工程配置文件
+        File configFile = new File(name);
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
+                callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+
+    private static void moneyGenerator() throws Exception {
+        GeneratorDisplay generatorSqlmap = new GeneratorDisplay();
+        generatorSqlmap.generator("generator-money/incomeGeneratorConfig.xml");
+//			generatorSqlmap = new GeneratorDisplay();
+        generatorSqlmap.generator("generator-money/balanceGeneratorConfig.xml");
+//			generatorSqlmap = new GeneratorDisplay();
+        generatorSqlmap.generator("generator-money/cashGeneratorConfig.xml");
+//			generatorSqlmap = new GeneratorDisplay();
+        generatorSqlmap.generator("generator-money/stockInfoGeneratorConfig.xml");
+    }
+
+    public static void main(String[] args) throws Exception {
+        try {
+            moneyGenerator();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
